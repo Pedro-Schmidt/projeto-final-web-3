3a54196e8652310c2312b912c8cd18b3b449ca1b
Pedro Schmidt <schmidthu3@outlook.com>
2020-06-26 19:04:31 -0300

    Created file

---
+++ biblioteca-frontend/src/components/Menu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="menu">
+    <h1></h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  props: {
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
