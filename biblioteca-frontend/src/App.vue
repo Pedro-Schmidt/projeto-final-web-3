81835243bf22f9bcc0998a26de5a526c2758860d
Pedro Schmidt <schmidthu3@outlook.com>
2020-06-26 22:30:28 -0300

    Created simple layout

--- biblioteca-frontend/src/App.vue
+++ biblioteca-frontend/src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Menu/>
+    
+    <router-view></router-view>
+
   </div>
 </template>
 
 <script>
-import Menu from './components/Menu.vue'
+
 
 export default {
   name: 'App',
   components: {
-    Menu
+    
   }
 }
 </script>
 
 <style>
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  width: 100%;
+  height: 100%;
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  font-size: 1.5vh;
   color: #2c3e50;
-  margin-top: 60px;
+  line-height: 4vh;
 }
 </style>
