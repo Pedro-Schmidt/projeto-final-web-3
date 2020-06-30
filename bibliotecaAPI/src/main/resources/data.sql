235ed1f072dd7d6cc0107910d6bb2fea8be9d76f
Pedro Schmidt <schmidthu3@outlook.com>
2020-06-30 14:51:03 -0300

    Changed

--- bibliotecaAPI/src/main/resources/data.sql
+++ bibliotecaAPI/src/main/resources/data.sql
@@ -1,5 +1,6 @@
+
 INSERT INTO books(book_name, book_author, book_illustrator, book_isbn, book_pages)
-VALUES("A escrava Isaura", "Bernardo Guimarães", "Ivan Pinheiro Machado", "978-85-254-0851-8", 175);
+VALUES('A escrava Isaura', 'Bernardo Guimarães', 'Ivan Pinheiro Machado', '978-85-254-0851-8', 175);
 
 INSERT INTO book_borrowing(return_date, name, phone_number, email, book_id)
-VALUES("26/05/2021", "Cleber Oliveira", 51999123456, "cleber@example.com", 1);
+VALUES('26/05/2021', 'Cleber Oliveira', 51999123456, 'cleber@example.com', 1);
