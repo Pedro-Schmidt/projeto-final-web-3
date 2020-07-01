60a1fe352263137c11f3be5fe5d2e594f1525eb1
Pedro Schmidt <schmidthu3@outlook.com>
2020-07-01 13:18:23 -0300

    Added user interaction with API

--- biblioteca-frontend/src/components/Books.vue
+++ biblioteca-frontend/src/components/Books.vue
@@ -6,54 +6,76 @@
     <main id="container">
         <h2>Pesquisar Livros</h2>
         <hr>
-        <form>
+        <form @submit.prevent="saveAndUpdateBook">
             <fieldset>
               <div class="lacuna">
                 <label for="nome-livro">Nome do livro</label>
-                <input type="text" id="nome-livro" name="nome-livro"><br>
+                <input type="text" id="nome-livro" v-model="book.nome" name="nome-livro"><br>
               </div>
               <div class="lacuna">
                 <label for="autor">Autor</label>
-                <input type="text" id="autor" name="autor"><br>
+                <input type="text" id="autor" v-model="book.autor" name="autor"><br>
               </div>
               <div class="lacuna">
                 <label for="ilustrador">Ilustrador</label>
-                <input type="text" id="ilustrador" name="ilustrador"><br>
+                <input type="text" id="ilustrador" v-model="book.ilustrador" name="ilustrador"><br>
               </div>
               <div class="lacuna">
                 <label for="ISBN">ISBN</label>
-                <input type="text" id="ISBN" name="ISBN"><br>
+                <input type="text" id="ISBN" v-model="book.isbn" name="ISBN"><br>
               </div>
               <div class="lacuna">
                 <label for="paginas">Páginas</label>
-                <input type="text" id="paginas" name="paginas"><br>
+                <input type="text" id="paginas" v-model="book.qntPaginas" name="paginas"><br>
               </div>
               <div class="botoes-form">
-                <input type="submit" value="Enviar">
-                <input type="reset">
+                <input type="submit" value="Salvar">
+                <input type="submit" v-show="book.id" @click="cancelUpdate" value="Cancelar">
               </div>
             </fieldset>
         </form>
         <hr>
         <section>
-          <table>
+          <p class="subtitulo">Quantidade de Livros: {{books.length}}</p>
+          <form @submit.prevent="findById">
+            <div class="lacuna">
+                <label for="pesquisa-codigo">Código</label>
+                <input type="text" id="pesquisa-codigo" v-model="book.id" name="pesquisa-codigo"><br>
+              </div>
+              <div class="botoes-form">
+                <input type="submit" value="Pesquisar">
+              </div>
+          </form>
+          <div v-show="findBook.id">
+            <p>Livro Encontrado<br>Código: {{findBook.id}} <br>Nome: {{findBook.nome}}<br>Autor: {{findBook.autor}}<br>Ilustrador: {{findBook.ilustrador}}<br>ISBN: {{findBook.isbn}}<br>Qnt. de Páginas: {{findBook.qntPaginas}} </p>
+          </div>
+          <table v-show="books.length > 0"><!--v-show="books.length > 0"-->
             <thead>
               <tr>
+                <th>Código</th>
                 <th>Nome</th>
                 <th>Autor</th>
                 <th>Ilustrador</th>
                 <th>ISBN</th>
                 <th>Qnt. de páginas</th>
+                <th>Excluir</th>
+                <th>Alterar</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>Body content 1</td>
-                <td>Body content 2</td>
-                <td>Body content 1</td>
-                <td>Body content 2</td>
-                <td>Body content 1</td>
-                
+              <tr v-for="book in books" :key="book.id">
+                <td>{{book.id}}</td>
+                <td>{{book.nome}}</td>
+                <td>{{book.autor}}</td>
+                <td>{{book.ilustrador}}</td>
+                <td>{{book.isbn}}</td>
+                <td>{{book.qntPaginas}}</td>
+                <td>
+                  <button class="botao" @click="deleteBook(book)">Excluir</button>
+                </td>
+                <td>
+                  <button class="botao" @click="updateBook(book)">Alterar</button>
+                </td>
               </tr>
             </tbody>
           </table>
@@ -64,14 +86,91 @@
 
 <script>
 import Menu from './Menu.vue';
+import BookServices from '../services/bookservices'
+
 
 export default {
+  data() {
+    return{
+      book: {
+        id: '',
+        nome: '',
+        autor: '',
+        ilustrador: '',
+        isbn: '',
+        qntPaginas: ''
+      },
+      findBook: {
+        id: '',
+        nome: '',
+        autor: '',
+        ilustrador: '',
+        isbn: '',
+        qntPaginas: ''
+      },
+      books: []
+    }
+  },
   name: 'Books',
   props: {
     
   },
   components: {
     Menu
+  },
+  mounted() {
+    this.listBooks();
+  },
+  methods: {
+    listBooks() {
+      BookServices.findAll().then(response => {
+        this.books = response.data;
+      });
+    },
+    saveAndUpdateBook() {
+      if (!this.book.id) {
+
+        BookServices.save(this.book).then(response => {
+          this.listBooks();
+          this.book = {}
+          alert("Livro Adicionado com sucesso");
+          return response;
+        })
+
+      } else {
+
+        BookServices.update(this.book).then(response => {
+          this.listBooks();
+          this.book = {}
+          alert("Livro atualizado com sucesso");
+          return response;
+        })
+
+      }
+    },
+    deleteBook(book) {
+      if(confirm("Deseja excluir o livro?")){
+        BookServices.delete(book).then(response => {
+        this.listBooks();
+        alert("Livro Excluido com sucesso")
+        return response;
+      })
+      }
+    },
+    findById() {
+      BookServices.findById(this.book).then( response => {
+        this.book = response.data;
+      })
+    },
+    updateBook(book) {
+
+      this.book = book;
+
+    },
+    calcelUpdate() {
+      this.book = {};
+      alert("Alteração cancelada");
+    }
   }
 }
 </script>
@@ -86,12 +185,21 @@ font-family: 'Poppins', sans-serif;
     height: 100%;
     
 }
-h2{
+h2, .subtitulo{
   font-size: 2em;
   line-height: 4rem;
   text-align: center;
   font-weight: 300;
   margin-top: 7vh;
+}
+table{
+  text-align: center;
+  border: 1pt solid rgba(98, 98, 98, 0.5);
+  width: 100%;
+}
+th, td{
+  width: 12.5%;
+  border: 1px solid rgba(98, 98, 98, 0.5);
 }
 form{
   margin: 1%;
@@ -152,8 +260,8 @@ form fieldset{
   text-align: center;
   
 }
-.botoes-form input{
-
+.botoes-form input, .botao{
+  text-align: center;
   -webkit-writing-mode: horizontal-tb !important;
   writing-mode: horizontal-tb;
   text-rendering: auto;
@@ -182,7 +290,7 @@ form fieldset{
   text-transform: uppercase;
   font-size: 0.8rem;
 }
-.botoes-form input:hover{
+.botoes-form input:hover, .botao:hover{
   border: 1pt solid rgba(0, 0, 0, 0.1);
   box-shadow: 0px 0px 5px 0px rgba(98, 98, 98, 0.75);
   background-color: #007bff;
@@ -190,7 +298,7 @@ form fieldset{
   outline: none;
   
 }
-.botoes-form input:active{
+.botoes-form input:active, .botao:active{
   border: 1pt solid rgba(0, 123, 255, 0.5);
   box-shadow: 0px 0px 5px 0px rgba(0, 123, 255, 0.75);
   outline: none;
