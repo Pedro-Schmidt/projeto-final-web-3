7afd3898ff1754344aa2330661147fa318ab188d
Pedro Schmidt <schmidthu3@outlook.com>
2020-07-01 08:22:46 -0300

    Added menu to the page

--- biblioteca-frontend/src/components/About.vue
+++ biblioteca-frontend/src/components/About.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="about">
-    
+    <menu/>
   </div>
 </template>
 
 <script>
+
+import Menu from './Menu.vue'
+
 export default {
   name: 'About',
   props: {
     
+  },
+  components: {
+      Menu
   }
 }
 </script>
